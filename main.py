ce75a50ad6aca2e17a26b94650e5ca55be9178cd
Alex <alex.dial@outlook.com>
2025-03-15 23:37:50 -0400

    created a stack

---
+++ main.py
@@ -0,0 +1,27 @@
+# stack based language Draevik interpreter.
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def clear(self):
+        self.stack = []
+
+    def __str__(self):
+        return str(self.stack)
